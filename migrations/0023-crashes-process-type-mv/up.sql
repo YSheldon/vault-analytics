3b19eaef6daac56854d5c54e1e84445f92bf5c34
Aubrey Keus <aubrey@brave.com>
2016-08-22 22:09:44 -0400

    Add materialized view grouped on process_type
      * Add materialized view with extra grouping on contents->>process_type
      * Add materialized view name to list in updater script
    
    Auditor: @bbondy

---
+++ migrations/0023-crashes-process-type-mv/up.sql
@@ -0,0 +1,22 @@
+CREATE MATERIALIZED VIEW dw.fc_crashes_process_type_mv AS
+SELECT
+  sp.to_ymd((contents->>'year_month_day'::text))             AS ymd,
+  COALESCE(contents->>'process_type', 'unknown')             AS process_type,
+  COALESCE(contents->>'_version', 'unknown')                 AS version,
+  COALESCE(contents->>'platform', 'unknown')                 AS platform,
+  COALESCE(contents->'metadata'->>'crash_reason', 'unknown') AS crash_reason,
+  COALESCE(contents->'metadata'->>'cpu', 'unknown')          AS cpu,
+  COALESCE(contents->'metadata'->>'signature', 'unknown')    AS signature,
+  COUNT(*)                                                   AS total
+FROM dtl.crashes
+WHERE
+  sp.to_ymd((contents->>'year_month_day'::text)) > (current_timestamp - '60 days'::interval)
+GROUP BY
+  sp.to_ymd((contents->>'year_month_day'::text)),
+  COALESCE(contents->>'process_type', 'unknown'),
+  COALESCE(contents->>'_version', 'unknown'),
+  COALESCE(contents->>'platform', 'unknown'),
+  COALESCE(contents->'metadata'->>'crash_reason', 'unknown'),
+  COALESCE(contents->'metadata'->>'cpu', 'unknown'),
+  COALESCE(contents->'metadata'->>'signature', 'unknown')
+;
