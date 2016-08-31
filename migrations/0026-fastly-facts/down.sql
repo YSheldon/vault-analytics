32101406d35b95832e2f178bfaeb440c8eb1db5f
Aubrey Keus <aubrey@brave.com>
2016-08-30 20:11:20 -0400

    DDL to add Fastly monthly active user table
      * Add Fastly monthly active user table
    
    Auditor: @bbondy

--- migrations/0026-fastly-facts/down.sql
+++ migrations/0026-fastly-facts/down.sql
@@ -1,1 +1,2 @@
 DROP TABLE dw.fc_fastly_usage;
+DROP TABLE dw.fc_fastly_calendar_month_usage;
--- migrations/0026-fastly-facts/up.sql
+++ migrations/0026-fastly-facts/up.sql
@@ -13,3 +13,18 @@ CREATE TABLE dw.fc_fastly_usage (
 ALTER TABLE dw.fc_fastly_usage ADD CONSTRAINT valid_channels CHECK ( channel IN ( 'dev', 'beta', 'stable' ) );
 ALTER TABLE dw.fc_fastly_usage ADD CONSTRAINT version_format CHECK ( version ~ '^\d+\.\d+\.\d+$' );
 ALTER TABLE dw.fc_fastly_usage ADD CONSTRAINT valid_platforms CHECK ( platform IN ( 'osx', 'winx64', 'android', 'ios', 'unknown', 'linux', 'winia32' ) );
+
+CREATE TABLE dw.fc_fastly_calendar_month_usage (
+  ymd          DATE    NOT NULL,
+  platform     TEXT    NOT NULL,
+  version      TEXT    NOT NULL,
+  channel      TEXT    NOT NULL,
+  country_code TEXT    NOT NULL,
+  dma          TEXT    NOT NULL,
+  total        BIGINT  NOT NULL DEFAULT 0,
+  PRIMARY KEY (ymd, platform, version, channel, country_code, dma)
+);
+
+ALTER TABLE dw.fc_fastly_calendar_month_usage ADD CONSTRAINT valid_channels CHECK ( channel IN ( 'dev', 'beta', 'stable' ) );
+ALTER TABLE dw.fc_fastly_calendar_month_usage ADD CONSTRAINT version_format CHECK ( version ~ '^\d+\.\d+\.\d+$' );
+ALTER TABLE dw.fc_fastly_calendar_month_usage ADD CONSTRAINT valid_platforms CHECK ( platform IN ( 'osx', 'winx64', 'android', 'ios', 'unknown', 'linux', 'winia32' ) );
