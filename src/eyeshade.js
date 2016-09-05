976d038cc458b6d7d0364587d5a169bfcea2a308
Aubrey Keus <aubrey@brave.com>
2016-09-05 14:07:41 -0400

    Add scripts to update Ledger / BTC values
      * Add script to periodically retrieve Ledger wallet info
      * Add script to periodically retrieve USD -> BTC quotes from Coinbase
      * Add library function to interface with Mongo eyeshade store
    
    Auditor: @bbondy

--- src/eyeshade.js
+++ src/eyeshade.js
@@ -1,26 +1,32 @@
-#!/usr/bin/env node
-
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-exports.walletsCreatedByDay = (db, cb) => {
-  var query = db.collection('wallets').aggregate([
-    {
-      $project: {
-        ymd: { $dateToString: { format: '%Y-%m-%d', date: '$timestamp' } }
+exports.wallets = (db, cb) => {
+  db.collection('wallets').find().toArray(function(err, items) {
+    if (err) {
+      throw err.toString()
+    }
+    var returnItems = items.map((item) => {
+      var toReturn = {
+        id: item._id,
+        created_ts: item._id.getTimestamp().getTime() / 1000,
+        updated_ts: item.timestamp.getHighBits(),
+        financial_balance: 0,
+        financial_spendable: 0,
+        financial_confirmed: 0,
+        financial_unconfirmed: 0
       }
-    },
-    {
-      $group: {
-        _id: {
-          ymd: '$ymd'
-        },
-        count: {
-          $sum: 1
-        }
+      toReturn.created = (new Date(toReturn.created_ts * 1000)).toISOString().slice(0, 10)
+      toReturn.updated = (new Date(toReturn.updated_ts * 1000)).toISOString().slice(0, 10)
+      if (item.balances) {
+        toReturn.financial_balance = item.balances.balance
+        toReturn.financial_spendable = item.balances.spendable
+        toReturn.financial_confirmed = item.balances.confirmed
+        toReturn.financial_unconfirmed = item.balances.unconfirmed
       }
-    },
-  ])
-  query.toArray(cb)
+      return toReturn
+    })
+    cb(err, returnItems)
+  })
 }
