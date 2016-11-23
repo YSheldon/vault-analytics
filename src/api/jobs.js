2559fc9b6dc26e7eab0086bf349b30e4a3910632
Aubrey Keus <aubrey@brave.com>
2016-11-22 19:18:28 -0500

    Add endpoint to provide last run time for scheduled jobs
    Auditors: @bbondy

--- src/api/jobs.js
+++ src/api/jobs.js
@@ -2,8 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-var _ = require('underscore')
-var assert = require('assert')
 var moment = require('moment')
 
 const JOB_STATUS = `
@@ -13,12 +11,36 @@ WHERE
   ts = ( SELECT MAX(ts) FROM dtl.runinfo WHERE id = RI.id )
 `
 
+const JOBS = `
+SELECT run_id, ts, id, duration
+FROM dtl.runinfo
+ORDER BY ts DESC
+`
+
 exports.setup = (server, client, mongo) => {
   server.route({
     method: 'GET',
     path: '/api/1/job_status',
     handler: function (request, reply) {
-      var jobName = null
+      client.query(JOB_STATUS, [], (err, results) => {
+        if (err) {
+          reply(err.toString()).code(500)
+        } else {
+          var rows = results.rows.map((row) => {
+            row.ago = moment(row.ts).fromNow()
+            row.duration = parseFloat(row.duration || 0)
+            return row
+          })
+          reply(rows)
+        }
+      })
+    }
+  })
+
+  server.route({
+    method: 'GET',
+    path: '/api/1/jobs',
+    handler: function (request, reply) {
       client.query(JOB_STATUS, [], (err, results) => {
         if (err) {
           reply(err.toString()).code(500)
