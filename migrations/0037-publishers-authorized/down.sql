75e1030d87b78f1612514b6b08dc2d6f7cc536ea
Aubrey Keus <aubrey@brave.com>
2017-04-26 13:47:32 -0400

    Add migration to support changes to publisher retrieval script
    Auditors: @bbondy

---
+++ migrations/0037-publishers-authorized/down.sql
@@ -0,0 +1,10 @@
+DROP TABLE dw.fc_daily_publishers;
+
+CREATE TABLE dw.fc_daily_publishers (
+  ymd         DATE           NOT NULL,
+  total       BIGINT         NOT NULL DEFAULT 0,
+  verified    BIGINT         NOT NULL DEFAULT 0,
+  address     BIGINT         NOT NULL DEFAULT 0,
+  irs         BIGINT         NOT NULL DEFAULT 0,
+  PRIMARY KEY (ymd)
+);
