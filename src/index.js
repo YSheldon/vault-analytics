f2929c786002cd1560649a5353ed0bec1adf0ce9
eV <ev@7pr.xyz>
2018-01-29 22:24:33 +0000

    add dotenv

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+require('dotenv').config()
+
 let path = require('path')
 let _ = require('underscore')
 
@@ -67,7 +69,7 @@ let kickoff = (err, connections) => {
   server.start((err) => {
     assert(!err, `error starting service ${err}`)
     console.log('Analytics service started')
-    slack.notify( { text: require('os').hostname() + ' ' + npminfo.name + '@' + npminfo.version + ' started' } )
+    slack.notify({ text: require('os').hostname() + ' ' + npminfo.name + '@' + npminfo.version + ' started' })
   })
 }
 
