bbd0e7dd69947942f34885d02737a8cfec580871
Aubrey Keus <aubrey@brave.com>
2018-02-16 13:21:18 -0500

    Add stats to referral promo panel

--- local/js/referral.js
+++ local/js/referral.js
@@ -15,14 +15,38 @@
     var results = await $.ajax("/api/1/referral/stats/summary")
     console.log(results)
 
+    var totalDownloads = _.reduce(results.platform_summary, (memo, summary) => { return memo + summary.downloads }, 0)
+
+    var summaryTable = $("#overview-referral-promo-summary")
+    summaryTable.append(`<tr><td>Participating Publishers</td><td><strong>${results.owner_summary.length}</strong></td></tr>`)
+    summaryTable.append(`<tr><td>Total Downloads</td><td><strong>${totalDownloads}</strong></td></tr>`)
+
     var platformTable = $("#overview-referral-promo-platform tbody")
-    results.platform_summary.forEach((summary) => {
+    let sortedPlatforms = results.platform_summary.sort((a, b) => {
+      return (b.downloads || 0) - (a.downloads || 0)
+    })
+    sortedPlatforms.forEach((summary) => {
       let finalizationPercentage = summary.downloads > 0 ?
         parseInt(summary.finalized / summary.downloads * 100) :
         0
-      console.log(finalizationPercentage)
-      platformTable.append(`<tr><td>${platformIconImg(summary.platform)}</td><td>${platformTitle(summary.platform)}</td><td class='text-right'>${summary.downloads}</td><td class='text-right'>${summary.finalized} <span class='subvalue'>${finalizationPercentage}%</span><td></tr>`)
+      let platformPercentage = parseInt(summary.downloads / totalDownloads * 100)
+      platformTable.append(`
+        <tr>
+          <td>${platformIconImg(summary.platform)}</td>
+          <td>${platformTitle(summary.platform)}</td>
+          <td class='text-right'>${summary.downloads} <span class='subvalue'>${platformPercentage}%</span></td>
+          <td class='text-right'>${summary.finalized} <span class='subvalue'>${finalizationPercentage}%</span><td>
+        </tr>
+      `)
     })
+    platformTable.append(`
+      <tr>
+        <td></td>
+        <td></td>
+        <td class='text-right'><strong>${totalDownloads}</strong></td>
+        <td class='text-right'></td>
+      </tr>
+    `)
 
     let sparkData = _.pluck(results.ymd_summary, 'downloads')
     console.log(sparkData)
@@ -39,7 +63,7 @@
       let finalizationPercentage = summary.downloads > 0 ?
         parseInt(summary.finalized / summary.downloads * 100) :
         0
-      tbl.append(`<tr><td>${summary.channel}</td><td>${platformIconImg(summary.platform)}</td><td>${platformTitle(summary.platform)}</td><td class='text-right'>${summary.downloads}</td><td class='text-right'>${summary.finalized} <span class='subvalue'>${finalizationPercentage}%</span><td></tr>`)
+      tbl.append(`<tr><td>${summary.title || summary.channel}</td><td>${platformIconImg(summary.platform)}</td><td>${platformTitle(summary.platform)}</td><td class='text-right'>${summary.downloads}</td><td class='text-right'>${summary.finalized} <span class='subvalue'>${finalizationPercentage}%</span><td></tr>`)
     })
   }
 
